eeeb043ae3a8a1bb9d07560c2364f8c1200342d3
Max <maksim-gutov@mail.ru>
2022-06-26 15:05:01 +0300

    Внесены изменения, которые были разобраны на созвоне

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-import {generateUserProfiles,generateTemplateElements} from './data.js';
+import {generateUserProfiles} from './data.js';
 import {USER_PROFILES_MAX_ID_COUNT} from './constants.js';
-generateTemplateElements();
+import {drawPictures} from './pictures.js';
+
+const profiles = generateUserProfiles(USER_PROFILES_MAX_ID_COUNT);
+drawPictures(profiles);
