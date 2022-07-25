d9aae34da5ede5b9b7a8f34bbcc0e1a486f7568d
maximys781 <43477150+maximys781@users.noreply.github.com>
2022-07-25 08:44:52 +0300

    Поправил сброс масштаба при загрузке нового фото

---
+++ scale.js
@@ -0,0 +1,42 @@
+import { MIN_SCALE_VALUE, MAX_SCALE_VALUE,CURRENT_VALUE } from './constants.js';
+const scaleSmaller = document.querySelector('.scale__control--smaller');
+const scaleBigger = document.querySelector('.scale__control--bigger');
+const bigImage = document.querySelector('.img-upload__preview');
+const scaleValueInput = document.querySelector('.scale__control--value');
+let currentValue = 100;
+
+function resetScale() {
+  scaleValueInput.value = `${CURRENT_VALUE}%`;
+  bigImage.style.transform = `scale(${CURRENT_VALUE}%)`;
+  return scaleValueInput;
+}
+
+function addEventScalePlus(step) {
+  scaleBigger.addEventListener('click', () => {
+    if (currentValue < MAX_SCALE_VALUE) {
+      currentValue = Math.max(currentValue + step, MIN_SCALE_VALUE);
+    }
+    scaleValueInput.value = `${currentValue}%`;
+    changeScaleImage(currentValue);
+  });
+}
+
+function addEventScaleMinus(step) {
+  scaleSmaller.addEventListener('click', () => {
+    if (currentValue > MIN_SCALE_VALUE) {
+      currentValue = Math.min(currentValue - step, MAX_SCALE_VALUE);
+    }
+    scaleValueInput.value = `${currentValue}%`;
+    changeScaleImage(currentValue);
+  });
+}
+
+function changeScaleImage() {
+  bigImage.style.transform = `scale(${currentValue}%)`;
+}
+
+function scaleEvents() {
+  addEventScalePlus(25);
+  addEventScaleMinus(25);
+}
+export { scaleEvents, resetScale };
